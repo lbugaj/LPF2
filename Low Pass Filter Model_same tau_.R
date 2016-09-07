24682cb3ed5e213396104f04efe55f1f78962d0a
lbugaj <l.j.bugaj@gmail.com>
2016-09-07 15:42:38 -0700

    Finished making multiple plots. Now need to make integrated surfaces

--- Low Pass Filter Model_same tau_.R
+++ Low Pass Filter Model_same tau_.R
@@ -86,7 +86,7 @@ pulseList = firstOrderRC(inputPeriodVals = 60,
                            inputPulseVals = c(10,20),
                            resistor = 2,
                            capacitor = 2,
-                           threshold = 0.65)
+                           threshold = 0.4)
 
 
 
@@ -94,16 +94,18 @@ pulseList = firstOrderRC(inputPeriodVals = 60,
 #automate a scanning R, C values, and pulse widths. make 3x3 matrix
 totModel = list()
 periodVals = 60
-pulseVals = c(10, 30, 60)
-rVals = c(0.2, 2, 20)
+pulseVals = c(10,20,30,40)
+rVals = c(2, 20, 40)
 cVal = 1
+thresh = 0.4
 
 for (i in 1:length(rVals)){
-    model = firstOrderRC(inputPeriodVals = periodVals,
+    
+  model = firstOrderRC(inputPeriodVals = periodVals,
                                        inputPulseVals = pulseVals,
                                        resistor = rVals[i],
                                        capacitor = cVal,
-                                       threshold = 0.65)
+                                       threshold = thresh)
     
     #model[[paste("int 60 pulse 10 R1_", rRise[i], " R0_", rFall[j] sep = "")]] = model
     namedModel = list()
@@ -112,80 +114,49 @@ for (i in 1:length(rVals)){
 }
 
 # now  graph that shit
-
-plotModels = function(list1){
-  par(mfrow=c(2,1))
-  for (i in 1:length(list1)){
-    plot(list1[[i]][[1]][,"time"],list1[[i]][[1]][,"model"], type = "l", col = "red", ylim = c(0,2), main = names(list1[i]))
+plotModels = function(list, columns, rows){
+  par(mfrow=c(columns,rows))
+  for (i in 1:length(list)){
+    plot(list[[i]][,"time"],list[[i]][,"models"], type = "l", col = "red", ylim = c(0,1), main = names(list[i]))
     #lines(list2[[i]][,"time"],list2[[i]][,"model"], col = "purple")
   }
 }
-plotIntegrals = function(list1){
-  par(mfrow=c(2,1))
-  for (i in 1:length(list1)){
-    plot(list1[[i]][[1]][,"time"],list1[[i]][[1]][,"integral"], type = "l", col = "red", ylim = c(1,1000), main = names(list1[i]))
-    #lines(list2[[i]][[1]][,"time"],list2[[i]][[1]][,"integral"], col = "purple")
+plotModels(totModel, columns = length(rVals), rows = length(pulseVals))
+
+plotThreshModels = function(list, columns, rows){
+  par(mfrow=c(columns, rows))
+  for (i in 1:length(list)){
+    plot(list[[i]][,"time"],list[[i]][,"anAbThresh"], type = "l", col = "red", ylim = c(0,1), main = names(list[i]))
+    #lines(list2[[i]][,"time"],list2[[i]][,"model"], col = "purple")
   }
 }
+plotThreshModels(totModel, columns = length(rVals), rows = length(pulseVals))
+
+plotDigModels = function(list, columns, rows){
+  par(mfrow=c(columns, rows))
+  for (i in 1:length(list)){
+    plot(list[[i]][,"time"],list[[i]][,"digAbThresh"], type = "l", col = "red", ylim = c(0,1), main = names(list[i]))
+    #lines(list2[[i]][,"time"],list2[[i]][,"model"], col = "purple")
+  }
+}
+plotDigModels(totModel, columns = length(rVals), rows = length(pulseVals))
+
+plotDigIntegrals = function(list, columns, rows){
+  par(mfrow=c(rows,columns))
+  for (i in 1:length(list)){
+    plot(list[[i]][,"time"],list[[i]][,"digAbThreshIntegral"], type = "l", col = "red", ylim = c(0,600), main = names(list[i]))
+    #lines(list2[[i]][,"time"],list2[[i]][,"model"], col = "purple")
+  }
+}
+plotDigIntegrals(totModel, columns = length(pulseVals), rows = length(rVals))
+
+
+#now make interpolated surfaces
+
+
 
 pdf("3_3 params model_CORRECT_60OFF_10ON.pdf",width=8,height=8)
 plotModels(totModel)
 dev.off()
 
-pdf("3_3 params integral_60OFF_30ON.pdf",width=8,height=8)
-plotIntegrals(totModel)
-dev.off()
-
-pdf("2_1 params model_60OFF_10ON.pdf",width=8,height=8)
-plotModels(totModel)
-dev.off()
-
-pdf("2_1 params integral_60OFF_10ON.pdf",width=8,height=8)
-plotIntegrals(totModel)
-dev.off()
-
-save.image("3x3 model matrix_correct model.RData")
-
-
-
-
-pdf("diffParam_r2c2r2c2.pdf",width=6,height=8)
-plotGrid(r2c2r2c2)
-dev.off()
-
-
-
-
-head(r2c2r2c2)
-plotGrid = function(list){
-  par(mfrow=c(3,2))
-  for (i in 1:length(list)){
-    plot(list[[i]][,"time"],list[[i]][,"model"], type = "l", ylim = c(0,1.4), main = names(list[i]))
-    plot(list[[i]][,"time"],list[[i]][,"integral"], type = "l", ylim = c(0,800), main = names(list[i]))
-  }
-}
-
-plotModels = function(list1, list2){
-  par(mfrow=c(3,1))
-  for (i in 1:length(list1)){
-    plot(list1[[i]][,"time"],list1[[i]][,"model"], type = "l", col = "red", main = names(list1[i]))
-    lines(list2[[i]][,"time"],list2[[i]][,"model"], col = "purple")
-  }
-}
-
-plotIntegrals = function(list1, list2){
-  par(mfrow=c(3,1))
-  for (i in 1:length(list1)){
-    plot(list1[[i]][,"time"],list1[[i]][,"integral"], type = "l", col = "red", ylim = c(1,800), main = names(list1[i]))
-    lines(list2[[i]][,"time"],list2[[i]][,"integral"], col = "purple")
-  }
-}
-
-plotThresh = function(list1, list2){
-  par(mfrow=c(3,1))
-  for (i in 1:length(list1)){
-    plot(list1[[i]][,"time"],list1[[i]][,"abThresh"], type = "l", col = "red", ylim = c(0,1.2), main = names(list1[i]))
-    lines(list2[[i]][,"time"],list2[[i]][,"abThresh"], col = "purple")
-  }
-}
 
